b41ed4fe7bf3996c022e5b1dab5bec79aa34d08a
mithem <mithem@github.com>
2024-05-15 13:24:53 +0200

    Add fps counter

--- gesture_glide/camera_handler.py
+++ gesture_glide/camera_handler.py
@@ -15,13 +15,24 @@ class FrameMetadata:
 class CameraHandler(Observable):
     def __init__(self):
         super().__init__()
+        self.frame_count = 0
 
     def run(self):
         capture = cv2.VideoCapture(0)
+        start_time = time.time()
+        last_frame_time = None
         while True:
             success, frame = capture.read()
             cv2.imshow('Capture', frame)
+
             metadata = FrameMetadata(width=frame.shape[0], height=frame.shape[1])
-            self.notify_observers(frame=frame, metadata=metadata)
+            time_from_last_frame = time.time() - last_frame_time if last_frame_time is not None else None
+            frame_rate = 1 / time_from_last_frame if time_from_last_frame else 0.0
+            self.notify_observers(frame=frame, metadata=metadata, frame_rate = frame_rate)
+            self.frame_count += 1
+            last_frame_time = time.time()
+            print("\r", "FPS:", frame_rate, end="")
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                seconds = time.time() - start_time
+                print(f"Read {self.frame_count} frames in {seconds} ({frame_rate :.2f} fps).")
                 break
